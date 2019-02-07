48417bed4fb603bd09e9798e193ea7e80c966818
geenkonzo <gcobisakonzo@gmail.com>
2019-02-07 16:52:17 +0200

    front end development of the booking app

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
+</head>
+<body>
+    <section class="grid-container">
+        <header>
+            <div class="top-grid">
+                <div class="logo">
+                    <img src="../img/logo.png">
+                </div>
+                <div class="greetings">
+                    <h1>Hello User!</h1>
+                </div>
+                <div class="logout">
+                    <button class="btn">Log Out</button>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="middle-grid">
+                <form>
+                    
+                </form>
+            </div>
+        </main>
+        <!-- <main>gallery</main> -->
+        <footer>footer</footer>
+    </section>
+</body>
+</html>
